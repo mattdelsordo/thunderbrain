c6c4496efbc75272976b822d6df08cbc3eb74d2f
Matt DelSordo <mattdelsordo@gmail.com>
2017-11-01 23:55:08 -0400

    whipped up a script to launch a full dev environment worth of terminal windows (for Windows)

---
+++ win_run_dev.bat
@@ -0,0 +1,4 @@
+cd "C:\Windows\System32"
+start bash.exe -c "cd ~/../mnt/c/Users/Matt\ DelSordo/Documents/Coursework/2017\ Fall/CSC\ 212/thunderbrain;bash"
+start bash.exe -c "cd ~/../mnt/c/Users/Matt\ DelSordo/Documents/Coursework/2017\ Fall/CSC\ 212/thunderbrain && yarn dev:wds;bash"
+start bash.exe -c "cd ~/../mnt/c/Users/Matt\ DelSordo/Documents/Coursework/2017\ Fall/CSC\ 212/thunderbrain && yarn dev:start;bash"
