67dd9bada2b75b52449a0d4ed00b734e06fbf9fb
LudwigNova <lnova@u.rochester.edu>
2017-11-29 17:12:58 -0500

    Made change to the way idea text is accessed before being sent to the voteIdea action

--- src/shared/container/voting-button.jsx
+++ src/shared/container/voting-button.jsx
@@ -8,8 +8,9 @@ import { voteIdea } from '../action/actions'
 const VotingButton = ({
   dispatch, idea, totalMembers, user,
 }: Props) => {
-  console.log(`idea: ${idea}`)
+  console.log(`idea: ${idea.get('text')}`)
   console.log(idea.get('points'))
+  console.log(`user: ${user}`)
   const threshold = (totalMembers / 2) || 0
   // console.log(`${idea.points.length}/${threshold}`)
   // TODO: theme these dynamically in a way that actually makes sense
@@ -18,7 +19,7 @@ const VotingButton = ({
       <div>
         <button
           onClick={() => {
-                    dispatch(voteIdea(idea, user))
+                    dispatch(voteIdea(idea.get('text'), user))
                 }}
           className="btn btn-primary m-4"
         >{`${idea.get('text')} +${idea.get('points').size}`}
@@ -30,7 +31,7 @@ const VotingButton = ({
     <div>
       <button
         onClick={() => {
-                    dispatch(voteIdea(idea, user))
+                    dispatch(voteIdea(idea.get('text'), user))
                 }}
         className="btn btn-secondary m-4"
       >{`${idea.get('text')} +${idea.get('points').size}`}
