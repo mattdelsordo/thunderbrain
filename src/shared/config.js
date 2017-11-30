a73455c219d2f5bc9996f8f09854b79d4733ab36
Matt DelSordo <mattdelsordo@gmail.com>
2017-11-30 00:14:57 -0500

    help

--- src/shared/config.js
+++ src/shared/config.js
@@ -10,7 +10,7 @@ export const APP_CONTAINER_CLASS = 'js-app'
 export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
 
 export const WEB_PORT = process.env.PORT || 8080
-export const SOCKET_PATH = 'https://thunderbrain.herokuapp.com/'
+export const SOCKET_PATH = 'https://thunderbrain.herokuapp.com:8080'
 export const STATIC_PATH = '/static'
 export const APP_NAME = 'ThunderBrain'
 
