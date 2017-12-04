b9ffb9f4f023558a9131f54300c4c04fa939c163
Matt DelSordo <mattdelsordo@gmail.com>
2017-12-04 00:13:42 -0500

    app now tells you if you have 0 voted on  results

--- src/shared/component/page/app/deliberation-page.jsx
+++ src/shared/component/page/app/deliberation-page.jsx
@@ -99,58 +99,67 @@ const DeliberationPage = ({
       )
     }
 
-    return (
-      <div>
-        <Helmet
-          title={title}
-          meta={[
-            { name: 'description', constent: title },
-            { property: 'og:title', content: title },
-          ]}
-        />
-        <AppNav />
-        <div className="container">
-          <div className="row">
-            <h1 className="mt-3">{`Prompt: "${topic}"`}</h1>
-          </div>
-          <div className="row">
-            <Clock time={deliberationSeconds} />
-            {username === host &&
-            <button
-              className="btn btn-primary m-3"
-              onClick={() => { dispatch(addDeliberationTime(roomID)) }}
-            >
-              30 More Seconds?
-            </button>}
-            {/*<button onClick={() => dispatch(leaveRoom())} className="btn btn-primary m-3">Exit Call</button>*/}
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSdhTWqdMZ-yeLi1o4SFUhJQ3OhAC2MW_7MVjQxgcplyPDC20w/viewform?usp=sf_link"
-              target="_blank"
-              rel="noopener"
-              onClick={() => dispatch(leaveRoom())}
-              className="btn btn-primary m-3"
-            >
-              Exit Call
-            </a>
-          </div>
-          <div className="col">
-            <h4>Results:</h4>
-            <ol className="list-group">
-              {ideas.sort((a, b) => {
-                if (a.get('points').size > b.get('points').size) return -1
-                else if (a.get('points').size < b.get('points').size) return 1
-                return 0
-              }).filter((idea, i) => {
-                return i < 5 && idea.get('points').size > 0
-              }).map((idea, i) => (<li key={i} className="list-group-item">{`${idea.get('text')} +${idea.get('points').size}`}</li>))}
-            </ol>
-          </div>
-          <div className="row">
-            <VideoChat roomID={roomID} />
+    else {
+      const results = ideas.sort((a, b) => {
+        if (a.get('points').size > b.get('points').size) return -1
+        else if (a.get('points').size < b.get('points').size) return 1
+        return 0
+      }).filter((idea, i) => {
+        return i < 5 && idea.get('points').size > 0
+      })
+
+      return (
+        <div>
+          <Helmet
+            title={title}
+            meta={[
+              { name: 'description', constent: title },
+              { property: 'og:title', content: title },
+            ]}
+          />
+          <AppNav />
+          <div className="container">
+            <div className="row">
+              <h1 className="mt-3">{`Prompt: "${topic}"`}</h1>
+            </div>
+            <div className="row">
+              <Clock time={deliberationSeconds} />
+              {username === host &&
+              <button
+                className="btn btn-primary m-3"
+                onClick={() => { dispatch(addDeliberationTime(roomID)) }}
+              >
+                30 More Seconds?
+              </button>}
+              {/*<button onClick={() => dispatch(leaveRoom())} className="btn btn-primary m-3">Exit Call</button>*/}
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSdhTWqdMZ-yeLi1o4SFUhJQ3OhAC2MW_7MVjQxgcplyPDC20w/viewform?usp=sf_link"
+                target="_blank"
+                rel="noopener"
+                onClick={() => dispatch(leaveRoom())}
+                className="btn btn-primary m-3"
+              >
+                Exit Call
+              </a>
+            </div>
+            <div className="col">
+              <h4>Results:</h4>
+              <ol className="list-group">
+                {
+                  ((results.length > 0)
+                  ? results.map((idea, i) => (<li key={i} className="list-group-item">{`${idea.get('text')} +${idea.get('points').size}`}</li>))
+                  : <h3>Nothing!</h3>)
+                }
+              </ol>
+            </div>
+            <div className="row">
+              <VideoChat roomID={roomID} />
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
+
   }
   console.log('ERROR: invalid redirect in lobby page')
   return (<Redirect to={PROFILE_VIEW} />)
